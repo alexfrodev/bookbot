5aa88fb1d58a1cd08fe9ed55b726df5f647ae7cb
alexfrodev <alex.franco3d@outlook.com>
2024-08-25 03:09:51 -0600

    add function to creater list of dictionaries

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     text = get_text()
-    print(word_count(text))
-    print(character_count(text))
+    words = word_count(text)
+    chars_dict = character_dict(text)
+    alpha_dict = is_alpha(chars_dict)
+    list = chars_list(alpha_dict)
+    print("----- Begin report of book 'frankenstein' -----")
+    print(f"{words} words found in the document")
+    print(list)
+    print("----- End report -----")
 
 
 def word_count(text):
@@ -9,7 +15,7 @@ def word_count(text):
     return words
 
 
-def character_count(text):
+def character_dict(text):
     character = {}
     for c in text.lower():
         if c in character:
@@ -17,6 +23,23 @@ def character_count(text):
         else:
             character[c] = 1
     return character
+
+
+def is_alpha(chars_dict):
+    alpha_chars = {}
+    for char in chars_dict:
+        if char.isalpha():
+            count = chars_dict[char]
+            alpha_chars[char] = count
+    
+    return alpha_chars
+
+
+def chars_list(alpha_dict):
+    #alpha_chars = is_alpha()
+    new_list = [{key:value} for key, value in alpha_dict.items()]
+    return new_list
+
 
 
 def get_text():
